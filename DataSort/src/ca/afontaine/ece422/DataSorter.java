ef18448d383083e3817dd4d5dbe73c38d9127c9b
Andrew Fontaine <andrew@afontaine.ca>
2014-10-19 14:27:22 -0600

    Finished DataGenerator.

--- DataSort/src/ca/afontaine/ece422/DataSorter.java
+++ DataSort/src/ca/afontaine/ece422/DataSorter.java
@@ -22,10 +22,169 @@
 
 package ca.afontaine.ece422;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Timer;
+import java.util.concurrent.*;
+
 /**
- * @author Andrew
+ * @author Andrew Fontaine
  * @version 1.0
  * @since 2014-10-12
  */
 public class DataSorter {
+
+	private static ExecutorService EXECUTOR = Executors.newSingleThreadExecutor();
+	private static Timer TIMER = new Timer();
+	private static String USAGE = "<input file> <output file> <error probability> <time limit>";
+	private static Random RANDOM = new Random();
+
+	private int time;
+	private double err;
+
+	public DataSorter(int time, double err) {
+		this.time = time;
+		this.err = err;
+	}
+
+	private boolean executeHeap(int[] arr) {
+		Future<Integer> heapSort = EXECUTOR.submit(() -> HeapSorter.sort(arr));
+		TIMER.schedule(new WatchDog(heapSort), time);
+		try {
+			int memory = heapSort.get();
+			double hazard = RANDOM.nextDouble();
+			if(hazard > 0.5 && hazard < 0.5 + err * memory) {
+				System.err.println("Heap sort failed due to memory errors.");
+				return false;
+			}
+			return new SortAdjudicator().adjudicate(arr);
+		}
+		catch(CancellationException | InterruptedException e) {
+			System.err.println("Heap Sort was cancelled by Watch Dog.");
+			return false;
+		}
+		catch(ExecutionException e) {
+			return false;
+		}
+	}
+
+	private boolean executeInsert(int[] arr) {
+		Future<Integer> insertionSort = EXECUTOR.submit(() -> NativeInsertionSorter.sort(arr));
+		TIMER.schedule(new WatchDog(insertionSort), time);
+
+		try {
+			int memory = insertionSort.get();
+			double hazard = RANDOM.nextDouble();
+			if(hazard > 0.5 && hazard < 0.5 + err * memory) {
+				System.err.println("Insertion sort failed due to memory errors.");
+				return false;
+			}
+			return new SortAdjudicator().adjudicate(arr);
+		}
+		catch(CancellationException | InterruptedException e) {
+			System.err.println("Insertion Sort was cancelled by Watch Dog.");
+			return false;
+		}
+		catch(ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private int[] execute(int[] arr) throws SortFailureException {
+		int[] arrHeap = Arrays.copyOf(arr, arr.length);
+		int[] arrInsert = Arrays.copyOf(arr, arr.length);
+		if(executeHeap(arrHeap)) return arrHeap;
+		else if(executeInsert(arrInsert)) return arrInsert;
+		throw new SortFailureException("Both sorting algorithms have failed.");
+	}
+
+
+	public static void main(String[] args) {
+		if(args.length != 4) {
+			System.err.println(USAGE);
+			System.exit(1);
+		}
+		String fileIn = args[0];
+		String fileOut = args[1];
+		int time = 0;
+		double err = 0.0;
+		String line = "";
+		int[] arr = new int[] {};
+		try {
+			err = Double.parseDouble(args[2]);
+		}
+		catch(NumberFormatException e) {
+			System.err.println("Argument: " + args[2] + " must be a decimal value.");
+			System.exit(1);
+		}
+		try {
+			time = Integer.parseInt(args[3]);
+			if(time < 1) {
+				System.err.println("Argument: " + args[3] + " must be greater than 0.");
+				System.exit(1);
+			}
+		}
+		catch(NumberFormatException e) {
+			System.err.println("Argument: " + args[3] + " must be an integer value.");
+			System.exit(1);
+		}
+		try {
+			File f = new File(args[0]);
+			f.getCanonicalPath();
+		}
+		catch(IOException e) {
+			System.err.println("Argument " + args[0] + " must be a valid filename");
+		}
+		try {
+			line = FileIO.read(fileIn);
+		}
+		catch(IOException e) {
+			System.err.println("Something went wrong reading the file.\n"
+					+ "Does it exist?\n"
+					+ "Do you have permission to read the file?");
+			System.exit(2);
+		}
+		try {
+			arr = Arrays.stream(line.split(",")).mapToInt(Integer::parseInt).toArray();
+		}
+		catch(NumberFormatException e) {
+			System.err.println(args[0] + " is not a file containing comma-separated integer values.");
+		}
+		try {
+			FileIO.write(args[1], new DataSorter(time, err).execute(arr));
+		}
+		catch(SortFailureException e) {
+			System.err.println(e.getMessage());
+			System.exit(3);
+		}
+		catch(IOException e) {
+			System.err.println("Cannot write to file. Check your privilege.");
+			new File(args[1]).delete();
+			System.exit(1);
+		}
+		System.exit(0);
+	}
+
+	private class SortAdjudicator {
+		public boolean adjudicate(int[] arr) {
+			boolean result = true;
+			for(int i = 0; i < arr.length - 1; i++) {
+				result &= arr[i] <= arr[i+1];
+			}
+			return result;
+		}
+
+	}
+
+	private class SortFailureException extends RuntimeException {
+		public SortFailureException() { super(); }
+		public SortFailureException(String message) { super(message); }
+		public SortFailureException(String message, Throwable cause) { super(message, cause); }
+		public SortFailureException(Throwable cause) { super(cause); }
+	}
 }
